a2fe468567fdb42f467ecbdad97d275637ae917c
Kien Nguyen <kien.n.quang@gmail.com>
2022-08-25 01:20:10 +0900

    ci: Update workflows to have nightly build and auto parsers update

--- .github/script/setup.ps1
+++ .github/script/setup.ps1
@@ -4,13 +4,4 @@
 # iwr -useb https://raw.githubusercontent.com/ScoopInstaller/Install/7bcdf996f0f4597ef69f4c48c219024a9512113c/install.ps1 | iex
 
 # XXX: https://github.com/ScoopInstaller/Install#for-admin
-iwr -useb https://get.scoop.sh -outfile 'install.ps1'
-.\install.ps1 -RunAsAdmin
-
-Join-Path (Resolve-Path ~).Path "scoop\shims" >> $env:GITHUB_PATH
-scoop bucket add extras
-scoop bucket add nonportable
-scoop update
-scoop checkup
-
-scoop install tar
+where.exe tar
