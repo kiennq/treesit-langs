338e49ad20569488603d20767358c1b5730735bd
Kien Nguyen <kien.n.quang@gmail.com>
2022-11-02 01:59:07 +0900

    queries/json: update highlight.scm

--- queries/json/highlights.scm
+++ queries/json/highlights.scm
@@ -1,13 +1,12 @@
 [(true) (false)] @boolean
 [(null)] @constant.builtin
-
 (number) @number
+
 (pair key: (string) @keyword)
 (pair value: (string) @string)
 
 (array (string) @string)
 (string_content (escape_sequence) @string.escape)
-(string_content) @string
 
 ["," ":"] @punctuation.delimiter
 ["{" "}" "[" "]"] @punctuation.bracket
