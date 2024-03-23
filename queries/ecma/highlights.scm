d4de02d45502e233fe2c833fe61932e611cca2d0
Kien Nguyen <kien.n.quang@gmail.com>
2024-03-23 12:52:12 -0700

    queries: update javascript and ecma

--- queries/ecma/highlights.scm
+++ queries/ecma/highlights.scm
@@ -1,106 +1,169 @@
 ; Types
-
 ; Javascript
-
 ; Variables
 ;-----------
 (identifier) @variable
 
 ; Properties
 ;-----------
+(property_identifier) @variable.member
 
-(property_identifier) @property
-(shorthand_property_identifier) @property
-(private_property_identifier) @property
+(shorthand_property_identifier) @variable.member
 
-(variable_declarator
-  name: (object_pattern
-    (shorthand_property_identifier_pattern))) @variable
+(private_property_identifier) @variable.member
+
+(object_pattern
+  (shorthand_property_identifier_pattern) @variable)
+
+(object_pattern
+  (object_assignment_pattern
+    (shorthand_property_identifier_pattern) @variable))
 
 ; Special identifiers
 ;--------------------
-
-((identifier) @constructor
- (#match? @constructor "^[A-Z]"))
+((identifier) @type
+  (#lua-match? @type "^[A-Z]"))
 
 ((identifier) @constant
- (#match? @constant "^[A-Z_][A-Z%d_]+$"))
+  (#lua-match? @constant "^_*[A-Z][A-Z%d_]*$"))
 
 ((shorthand_property_identifier) @constant
- (#match? @constant "^[A-Z_][A-Z%d_]+$"))
+  (#lua-match? @constant "^_*[A-Z][A-Z%d_]*$"))
 
 ((identifier) @variable.builtin
- (#match? @variable.builtin "^(arguments|module|console|window|document)$"))
+  (#any-of? @variable.builtin "arguments" "module" "console" "window" "document"))
 
-((identifier) @function.builtin
- (#eq? @function.builtin "require"))
+((identifier) @type.builtin
+  (#any-of? @type.builtin
+    "Object" "Function" "Boolean" "Symbol" "Number" "Math" "Date" "String" "RegExp" "Map" "Set"
+    "WeakMap" "WeakSet" "Promise" "Array" "Int8Array" "Uint8Array" "Uint8ClampedArray" "Int16Array"
+    "Uint16Array" "Int32Array" "Uint32Array" "Float32Array" "Float64Array" "ArrayBuffer" "DataView"
+    "Error" "EvalError" "InternalError" "RangeError" "ReferenceError" "SyntaxError" "TypeError"
+    "URIError"))
+
+(statement_identifier) @label
 
 ; Function and method definitions
 ;--------------------------------
-
-(function
+(function_expression
   name: (identifier) @function)
+
 (function_declaration
   name: (identifier) @function)
+
 (generator_function
   name: (identifier) @function)
+
 (generator_function_declaration
   name: (identifier) @function)
+
 (method_definition
-  name: [(property_identifier) (private_property_identifier)] @method)
+  name: [
+    (property_identifier)
+    (private_property_identifier)
+  ] @function.method)
+
+(method_definition
+  name: (property_identifier) @constructor
+  (#eq? @constructor "constructor"))
 
 (pair
-  key: (property_identifier) @method
-  value: (function))
+  key: (property_identifier) @function.method
+  value: (function_expression))
+
 (pair
-  key: (property_identifier) @method
+  key: (property_identifier) @function.method
   value: (arrow_function))
 
 (assignment_expression
   left: (member_expression
-    property: (property_identifier) @method)
+    property: (property_identifier) @function.method)
   right: (arrow_function))
+
 (assignment_expression
   left: (member_expression
-    property: (property_identifier) @method)
-  right: (function))
+    property: (property_identifier) @function.method)
+  right: (function_expression))
 
 (variable_declarator
   name: (identifier) @function
   value: (arrow_function))
+
 (variable_declarator
   name: (identifier) @function
-  value: (function))
+  value: (function_expression))
 
 (assignment_expression
   left: (identifier) @function
   right: (arrow_function))
+
 (assignment_expression
   left: (identifier) @function
-  right: (function))
+  right: (function_expression))
 
 ; Function and method calls
 ;--------------------------
-
 (call_expression
-  function: (identifier) @function)
+  function: (identifier) @function.call)
 
 (call_expression
   function: (member_expression
-    property: [(property_identifier) (private_property_identifier)] @method))
+    property: [
+      (property_identifier)
+      (private_property_identifier)
+    ] @function.method.call))
+
+; Builtins
+;---------
+((identifier) @module.builtin
+  (#eq? @module.builtin "Intl"))
+
+((identifier) @function.builtin
+  (#any-of? @function.builtin
+    "eval" "isFinite" "isNaN" "parseFloat" "parseInt" "decodeURI" "decodeURIComponent" "encodeURI"
+    "encodeURIComponent" "require"))
+
+; Constructor
+;------------
+(new_expression
+  constructor: (identifier) @constructor)
 
 ; Variables
 ;----------
 (namespace_import
-  (identifier) @namespace)
+  (identifier) @module)
+
+; Decorators
+;----------
+(decorator
+  "@" @attribute
+  (identifier) @attribute)
+
+(decorator
+  "@" @attribute
+  (call_expression
+    (identifier) @attribute))
+
+(decorator
+  "@" @attribute
+  (member_expression
+    (property_identifier) @attribute))
+
+(decorator
+  "@" @attribute
+  (call_expression
+    (member_expression
+      (property_identifier) @attribute)))
 
 ; Literals
 ;---------
-
 [
   (this)
   (super)
 ] @variable.builtin
+
+((identifier) @variable.builtin
+  (#eq? @variable.builtin "self"))
 
 [
   (true)
@@ -108,35 +171,49 @@
 ] @boolean
 
 [
-  (null) 
+  (null)
   (undefined)
 ] @constant.builtin
 
 [
   (comment)
-  (hash_bang_line)
-] @comment
+  (html_comment)
+] @comment @spell
+
+((comment) @comment.documentation
+  (#lua-match? @comment.documentation "^/[*][*][^*].*[*]/$"))
+
+(hash_bang_line) @keyword.directive
+
+((string_fragment) @keyword.directive
+  (#eq? @keyword.directive "use strict"))
 
 (string) @string
+
 (template_string) @string
+
 (escape_sequence) @string.escape
-(regex_pattern) @string.regex
-(regex "/" @punctuation.bracket) ; Regex delimiters
+
+(regex_pattern) @string.regexp
+
+(regex_flags) @character.special
+
+(regex
+  "/" @punctuation.bracket) ; Regex delimiters
 
 (number) @number
+
 ((identifier) @number
   (#any-of? @number "NaN" "Infinity"))
 
 ; Punctuation
 ;------------
-
-"..." @punctuation.special
-
-[";"
- "."
- "," ] @punctuation.delimiter
-
-(pair ":" @punctuation.delimiter)
+[
+  ";"
+  "."
+  ","
+  ":"
+] @punctuation.delimiter
 
 [
   "--"
@@ -180,11 +257,31 @@
   "&&="
   "||="
   "??="
+  "..."
 ] @operator
 
-(binary_expression "/" @operator)
-(ternary_expression ["?" ":"] @conditional)
-(unary_expression ["!" "~" "-" "+" "delete" "void" "typeof"]  @operator)
+(binary_expression
+  "/" @operator)
+
+(ternary_expression
+  [
+    "?"
+    ":"
+  ] @keyword.conditional.ternary)
+
+(unary_expression
+  [
+    "!"
+    "~"
+    "-"
+    "+"
+  ] @operator)
+
+(unary_expression
+  [
+    "delete"
+    "void"
+  ] @keyword.operator)
 
 [
   "("
@@ -195,73 +292,91 @@
   "}"
 ] @punctuation.bracket
 
-((template_substitution ["${" "}"] @punctuation.special) @none)
+(template_substitution
+  [
+    "${"
+    "}"
+  ] @punctuation.special) @none
 
 ; Keywords
 ;----------
+[
+  "if"
+  "else"
+  "switch"
+  "case"
+] @keyword.conditional
 
 [
-"if"
-"else"
-"switch"
-"case"
-"default"
-] @conditional
+  "import"
+  "from"
+] @keyword.import
+
+(export_specifier
+  "as" @keyword.import)
+
+(import_specifier
+  "as" @keyword.import)
+
+(namespace_export
+  "as" @keyword.import)
+
+(namespace_import
+  "as" @keyword.import)
 
 [
-"import"
-"from"
-"as"
-] @include
+  "for"
+  "of"
+  "do"
+  "while"
+  "continue"
+] @keyword.repeat
 
 [
-"for"
-"of"
-"do"
-"while"
-"continue"
-] @repeat
-
-[
-"async"
-"await"
-"break"
-"class"
-"const"
-"debugger"
-"export"
-"extends"
-"get"
-"in"
-"instanceof"
-"let"
-"set"
-"static"
-"switch"
-"target"
-"typeof"
-"var"
-"void"
-"with"
+  "break"
+  "class"
+  "const"
+  "debugger"
+  "export"
+  "extends"
+  "get"
+  "let"
+  "set"
+  "static"
+  "target"
+  "var"
+  "with"
 ] @keyword
 
 [
-"return"
-"yield"
+  "async"
+  "await"
+] @keyword.coroutine
+
+[
+  "return"
+  "yield"
 ] @keyword.return
 
-[
- "function"
-] @keyword.function
+"function" @keyword.function
 
 [
- "new"
- "delete"
+  "new"
+  "delete"
+  "in"
+  "instanceof"
+  "typeof"
 ] @keyword.operator
 
 [
- "throw"
- "try"
- "catch"
- "finally"
-] @exception
+  "throw"
+  "try"
+  "catch"
+  "finally"
+] @keyword.exception
+
+(export_statement
+  "default" @keyword)
+
+(switch_default
+  "default" @keyword.conditional)
--- queries/javascript/highlights.scm
+++ queries/javascript/highlights.scm
@@ -1,76 +1,56 @@
 ; inherits: ecma,jsx
 
-(optional_chain) @punctuation.delimiter
-
-;;; Parameters
-(formal_parameters (identifier) @variable.parameter)
+; Parameters
+(formal_parameters
+  (identifier) @variable.parameter)
 
 (formal_parameters
   (rest_pattern
     (identifier) @variable.parameter))
 
-;; ({ a }) => null
+; ({ a }) => null
 (formal_parameters
   (object_pattern
     (shorthand_property_identifier_pattern) @variable.parameter))
 
-;; ({ a: b }) => null
+; ({ a = b }) => null
+(formal_parameters
+  (object_pattern
+    (object_assignment_pattern
+      (shorthand_property_identifier_pattern) @variable.parameter)))
+
+; ({ a: b }) => null
 (formal_parameters
   (object_pattern
     (pair_pattern
       value: (identifier) @variable.parameter)))
 
-;; ([ a ]) => null
+; ([ a ]) => null
 (formal_parameters
   (array_pattern
     (identifier) @variable.parameter))
 
-;; Function and method definitions
+; ({ a } = { a }) => null
+(formal_parameters
+  (assignment_pattern
+    (object_pattern
+      (shorthand_property_identifier_pattern) @variable.parameter)))
 
-(function
- name: (identifier) @function)
-(function_declaration
- name: (identifier) @function)
-(method_definition
- name: (property_identifier) @method)
+; ({ a = b } = { a }) => null
+(formal_parameters
+  (assignment_pattern
+    (object_pattern
+      (object_assignment_pattern
+        (shorthand_property_identifier_pattern) @variable.parameter))))
 
-(variable_declarator
- name: (identifier) @function
- value: [(function) (arrow_function)])
-
-(assignment_expression
- left: [(identifier) @function
-        (member_expression property: (property_identifier) @method)]
- right: [(function) (arrow_function)])
-
-(pair key: (property_identifier) @method
-      value: [(function) (arrow_function)])
-
-;; Function and method calls
-
-(call_expression
- function: [(identifier) @function.call
-            (member_expression
-             property: (property_identifier) @method.call)])
-
-;; Variables
-
-(variable_declarator
- name: (identifier) @variable)
-(assignment_expression
- left: [(identifier) @variable
-        (member_expression property: (property_identifier) @variable)])
-(augmented_assignment_expression
- left: [(identifier) @variable
-        (member_expression property: (property_identifier) @variable)])
-(for_in_statement
- left: (identifier) @variable)
-
-;; a => null
+; a => null
 (arrow_function
   parameter: (identifier) @variable.parameter)
 
-;; optional parameters
+; optional parameters
 (formal_parameters
   (assignment_pattern
     left: (identifier) @variable.parameter))
+
+; punctuation
+(optional_chain) @punctuation.delimiter
