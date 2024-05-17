c1435d386f410e154cb8247b5bc8d9d2235913d7
Kien Nguyen <kien.n.quang@gmail.com>
2024-05-17 19:20:40 +0000

    c-sharp: update queries

--- queries/c-sharp/highlights.scm
+++ queries/c-sharp/highlights.scm
@@ -1,5 +1,14 @@
-((identifier) @variable.builtin
-  (#eq? @variable.builtin "_"))
+[
+  (identifier)
+  (preproc_arg)
+] @variable
+
+((preproc_arg) @constant.macro
+  (#lua-match? @constant.macro "^[_A-Z][_A-Z0-9]*$"))
+
+((identifier) @keyword
+  (#eq? @keyword "value")
+  (#has-ancestor? @keyword accessor_declaration))
 
 (method_declaration
   name: (identifier) @function.method)
@@ -8,7 +17,22 @@
   name: (identifier) @function.method)
 
 (method_declaration
+  returns: [
+    (identifier) @type
+    (generic_name
+      (identifier) @type)
+  ])
+
+(event_declaration
   type: (identifier) @type)
+
+(event_declaration
+  name: (identifier) @variable.member)
+
+(event_field_declaration
+  (variable_declaration
+    (variable_declarator
+      name: (identifier) @variable.member)))
 
 (declaration_pattern
   type: (identifier) @type)
@@ -26,28 +50,30 @@
     name: (identifier) @function.method.call))
 
 (invocation_expression
-  function:
-    (conditional_access_expression
-      (member_binding_expression
-        name: (identifier) @function.method.call)))
+  function: (conditional_access_expression
+    (member_binding_expression
+      name: (identifier) @function.method.call)))
 
 (namespace_declaration
-  name:
-    [
-      (qualified_name)
-      (identifier)
-    ] @module)
+  name: [
+    (qualified_name)
+    (identifier)
+  ] @module)
 
 (qualified_name
   (identifier) @type)
 
 (namespace_declaration
-  name:
-    [
-      (qualified_name
-        (identifier) @module)
-      (identifier) @module
-    ])
+  name: (identifier) @module)
+
+(file_scoped_namespace_declaration
+  name: (identifier) @module)
+
+(qualified_name
+  (identifier) @module
+  (#not-has-ancestor? @module method_declaration)
+  (#not-has-ancestor? @module record_declaration)
+  (#has-ancestor? @module namespace_declaration file_scoped_namespace_declaration))
 
 (invocation_expression
   (identifier) @function.method.call)
@@ -61,13 +87,16 @@
   (assignment_expression
     left: (identifier) @variable.member))
 
-(parameter_list
-  (parameter
-    name: (identifier) @variable.parameter))
+(parameter
+  name: (identifier) @variable.parameter)
 
-(implicit_parameter_list
-  (parameter
-    name: (identifier) @variable.parameter))
+(parameter_list
+  name: (identifier) @variable.parameter)
+
+(bracketed_parameter_list
+  name: (identifier) @variable.parameter)
+
+(implicit_parameter) @variable.parameter
 
 (parameter_list
   (parameter
@@ -79,15 +108,30 @@
 
 (null_literal) @constant.builtin
 
+(calling_convention
+  [
+    (identifier)
+    "Cdecl"
+    "Stdcall"
+    "Thiscall"
+    "Fastcall"
+  ] @attribute.builtin)
+
 (character_literal) @character
 
 [
   (string_literal)
+  (raw_string_literal)
   (verbatim_string_literal)
   (interpolated_string_expression)
 ] @string
 
-(boolean_literal) @boolean
+(escape_sequence) @string.escape
+
+[
+  "true"
+  "false"
+] @boolean
 
 (predefined_type) @type.builtin
 
@@ -108,8 +152,7 @@
   (identifier) @type)
 
 (using_directive
-  (name_equals
-    (identifier) @type.definition))
+  (type) @type.definition)
 
 (property_declaration
   name: (identifier) @property)
@@ -118,7 +161,16 @@
   type: (identifier) @type)
 
 (nullable_type
-  (identifier) @type)
+  type: (identifier) @type)
+
+(array_type
+  type: (identifier) @type)
+
+(ref_type
+  type: (identifier) @type)
+
+(pointer_type
+  type: (identifier) @type)
 
 (catch_declaration
   type: (identifier) @type)
@@ -132,10 +184,46 @@
 (record_declaration
   name: (identifier) @type)
 
+(struct_declaration
+  name: (identifier) @type)
+
 (enum_declaration
   name: (identifier) @type)
 
+(enum_member_declaration
+  name: (identifier) @variable.member)
+
+(operator_declaration
+  type: (identifier) @type)
+
+(conversion_operator_declaration
+  type: (identifier) @type)
+
+(explicit_interface_specifier
+  [
+    (identifier) @type
+    (generic_name
+      (identifier) @type)
+  ])
+
+(explicit_interface_specifier
+  (identifier) @type)
+
+(primary_constructor_base_type
+  type: (identifier) @type)
+
+[
+  "assembly"
+  "module"
+  "this"
+  "base"
+  (discard)
+] @variable.builtin
+
 (constructor_declaration
+  name: (identifier) @constructor)
+
+(destructor_declaration
   name: (identifier) @constructor)
 
 (constructor_initializer
@@ -148,7 +236,7 @@
   (identifier) @type)
 
 ; Generic Types.
-(type_of_expression
+(typeof_expression
   (generic_name
     (identifier) @type))
 
@@ -160,9 +248,13 @@
   (generic_name
     (identifier) @type))
 
-(type_constraint
-  (generic_name
-    (identifier) @type))
+(type_parameter_constraint
+  [
+    (identifier) @type
+    (type
+      (generic_name
+        (identifier) @type))
+  ])
 
 (object_creation_expression
   (generic_name
@@ -173,16 +265,14 @@
     (identifier) @type))
 
 (_
-  type:
-    (generic_name
-      (identifier) @type))
+  type: (generic_name
+    (identifier) @type))
 
 ; Generic Method invocation with generic type
 (invocation_expression
-  function:
-    (generic_name
-      .
-      (identifier) @function.method.call))
+  function: (generic_name
+    .
+    (identifier) @function.method.call))
 
 (invocation_expression
   (member_access_expression
@@ -198,14 +288,25 @@
 (type_parameter_list
   (type_parameter) @type)
 
+(type_parameter
+  name: (identifier) @type)
+
 (type_parameter_constraints_clause
-  target: (identifier) @type)
+  "where"
+  .
+  (identifier) @type)
 
 (attribute
   name: (identifier) @attribute)
 
-(for_each_statement
+(foreach_statement
   type: (identifier) @type)
+
+(goto_statement
+  (identifier) @label)
+
+(labeled_statement
+  (identifier) @label)
 
 (tuple_element
   type: (identifier) @type)
@@ -215,42 +316,47 @@
     (declaration_expression
       type: (identifier) @type)))
 
+(cast_expression
+  type: (identifier) @type)
+
+(lambda_expression
+  type: (identifier) @type)
+
 (as_expression
   right: (identifier) @type)
 
-(type_of_expression
+(typeof_expression
   (identifier) @type)
 
-(name_colon
-  (identifier) @variable.parameter)
-
-(warning_directive) @comment.warning
-
-(error_directive) @keyword.exception
-
-(define_directive
-  (identifier) @constant) @constant.macro
-
-(undef_directive
-  (identifier) @constant) @constant.macro
-
-(line_directive) @constant.macro
-
-(line_directive
-  (preproc_integer_literal) @constant
-  (preproc_string_literal)? @string)
-
-(pragma_directive
-  (identifier) @constant) @constant.macro
-
-(pragma_directive
-  (preproc_string_literal) @string) @constant.macro
+(preproc_error) @keyword.exception
 
 [
-  (nullable_directive)
-  (region_directive)
-  (endregion_directive)
+  "#define"
+  "#undef"
+] @keyword.directive.define
+
+[
+  "#if"
+  "#elif"
+  "#else"
+  "#endif"
+  "#region"
+  "#endregion"
+  "#line"
+  "#pragma"
+  "#nullable"
+  "#error"
+  (shebang_directive)
+] @keyword.directive
+
+[
+  (preproc_line)
+  (preproc_pragma)
+  (preproc_nullable)
 ] @constant.macro
+
+(preproc_pragma
+  (identifier) @constant)
 
 [
   "if"
@@ -259,16 +365,15 @@
   "break"
   "case"
   "when"
-  (if_directive)
-  (elif_directive)
-  (else_directive)
-  (endif_directive)
 ] @keyword.conditional
 
-(if_directive
+(preproc_pragma
   (identifier) @constant)
 
-(elif_directive
+(preproc_if
+  (identifier) @constant)
+
+(preproc_if
   (identifier) @constant)
 
 [
@@ -328,6 +433,7 @@
   "=>"
   "??"
   "??="
+  ".."
 ] @operator
 
 [
@@ -352,17 +458,13 @@
   ")"
 ] @punctuation.bracket
 
+(interpolation_brace) @punctuation.special
+
 (type_argument_list
   [
     "<"
     ">"
   ] @punctuation.bracket)
-
-[
-  (this_expression)
-  (base_expression)
-  "this"
-] @variable.builtin
 
 [
   "using"
@@ -383,6 +485,9 @@
   "or"
   "not"
   "stackalloc"
+  "__makeref"
+  "__reftype"
+  "__refvalue"
   "in"
   "out"
   "ref"
@@ -396,25 +501,34 @@
   "implicit"
   "explicit"
   "override"
-  "class"
-  "delegate"
-  "enum"
-  "interface"
-  "namespace"
-  "struct"
   "get"
   "set"
   "init"
   "where"
-  "record"
-  "event"
   "add"
   "remove"
   "checked"
   "unchecked"
   "fixed"
   "alias"
+  "file"
+  "unsafe"
 ] @keyword
+
+(attribute_target_specifier
+  .
+  _ @keyword)
+
+[
+  "enum"
+  "record"
+  "class"
+  "struct"
+  "interface"
+  "namespace"
+  "event"
+  "delegate"
+] @keyword.type
 
 [
   "async"
@@ -428,9 +542,9 @@
   "static"
   "volatile"
   "required"
-] @keyword.storage
-
-[
+  "managed"
+  "unmanaged"
+  "notnull"
   "abstract"
   "private"
   "protected"
@@ -439,9 +553,11 @@
   "partial"
   "sealed"
   "virtual"
-] @type.qualifier
+  "global"
+] @keyword.modifier
 
-(parameter_modifier) @operator
+(scoped_type
+  "scoped" @keyword.modifier)
 
 (query_expression
   (_
