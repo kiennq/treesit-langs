3bf569fdc4eace1b0fe484ea1f0ad19856dc6c6c
Kien Nguyen <kien.n.quang@gmail.com>
2024-06-13 13:30:26 -0700

    highlight: update for go/rust

--- queries/go/highlights.scm
+++ queries/go/highlights.scm
@@ -1,5 +1,30 @@
-; Function calls
+; Forked from tree-sitter-go
+; Copyright (c) 2014 Max Brunsfeld (The MIT License)
+;
+; Identifiers
+(type_identifier) @type
 
+(type_spec
+  name: (type_identifier) @type.definition)
+
+(field_identifier) @property
+
+(identifier) @variable
+
+(package_identifier) @module
+
+(parameter_declaration
+  (identifier) @variable.parameter)
+
+(variadic_parameter_declaration
+  (identifier) @variable.parameter)
+
+(label_name) @label
+
+(const_spec
+  name: (identifier) @constant)
+
+; Function calls
 (call_expression
   function: (identifier) @function.call)
 
@@ -8,25 +33,25 @@
     field: (field_identifier) @function.method.call))
 
 ; Function definitions
-
 (function_declaration
   name: (identifier) @function)
 
 (method_declaration
   name: (field_identifier) @function.method)
 
-; Identifiers
+(method_elem
+  name: (field_identifier) @function.method)
 
-(type_identifier) @type
-(field_identifier) @property
-(short_var_declaration left: (expression_list (identifier) @variable))
-(var_declaration (var_spec name: (identifier) @variable))
-(parameter_declaration (identifier) @variable.parameter)
-(const_declaration (const_spec name: (identifier) @variable))
-(range_clause left: (expression_list (identifier) @variable))
+; Constructors
+((call_expression
+  (identifier) @constructor)
+  (#lua-match? @constructor "^[nN]ew.+$"))
+
+((call_expression
+  (identifier) @constructor)
+  (#lua-match? @constructor "^[mM]ake.+$"))
 
 ; Operators
-
 [
   "--"
   "-"
@@ -43,6 +68,8 @@
   "&"
   "&&"
   "&="
+  "&^"
+  "&^="
   "%"
   "%="
   "^"
@@ -68,56 +95,145 @@
 ] @operator
 
 ; Keywords
-
 [
   "break"
-  "case"
-  "chan"
   "const"
   "continue"
   "default"
   "defer"
-  "else"
-  "fallthrough"
-  "for"
-  "func"
-  "go"
   "goto"
-  "if"
-  "import"
-  "interface"
-  "map"
-  "package"
   "range"
-  "return"
   "select"
-  "struct"
-  "switch"
-  "type"
   "var"
+  "fallthrough"
 ] @keyword
 
+[
+  "type"
+  "struct"
+  "interface"
+] @keyword.type
+
+"func" @keyword.function
+
+"return" @keyword.return
+
+"go" @keyword.coroutine
+
+"for" @keyword.repeat
+
+[
+  "import"
+  "package"
+] @keyword.import
+
+[
+  "else"
+  "case"
+  "switch"
+  "if"
+] @keyword.conditional
+
+; Builtin types
+[
+  "chan"
+  "map"
+] @type.builtin
+
+((type_identifier) @type.builtin
+  (#any-of? @type.builtin
+    "any" "bool" "byte" "comparable" "complex128" "complex64" "error" "float32" "float64" "int"
+    "int16" "int32" "int64" "int8" "rune" "string" "uint" "uint16" "uint32" "uint64" "uint8"
+    "uintptr"))
+
+; Builtin functions
+((identifier) @function.builtin
+  (#any-of? @function.builtin
+    "append" "cap" "clear" "close" "complex" "copy" "delete" "imag" "len" "make" "max" "min" "new"
+    "panic" "print" "println" "real" "recover"))
+
+; Delimiters
+"." @punctuation.delimiter
+
+"," @punctuation.delimiter
+
+":" @punctuation.delimiter
+
+";" @punctuation.delimiter
+
+"(" @punctuation.bracket
+
+")" @punctuation.bracket
+
+"{" @punctuation.bracket
+
+"}" @punctuation.bracket
+
+"[" @punctuation.bracket
+
+"]" @punctuation.bracket
+
 ; Literals
+(interpreted_string_literal) @string
 
-[
-  (interpreted_string_literal)
-  (raw_string_literal)
-  (rune_literal)
-] @string
+(raw_string_literal) @string
 
-(escape_sequence) @escape
+(rune_literal) @string
 
-[
-  (int_literal)
-  (float_literal)
-  (imaginary_literal)
-] @number
+(escape_sequence) @string.escape
+
+(int_literal) @number
+
+(float_literal) @number.float
+
+(imaginary_literal) @number
 
 [
   (true)
   (false)
+] @boolean
+
+[
   (nil)
   (iota)
 ] @constant.builtin
 
-(comment) @comment
+(keyed_element
+  .
+  (literal_element
+    (identifier) @variable.member))
+
+(field_declaration
+  name: (field_identifier) @variable.member)
+
+; Comments
+(comment) @comment @spell
+
+; Doc Comments
+(source_file
+  .
+  (comment)+ @comment.documentation)
+
+(source_file
+  (comment)+ @comment.documentation
+  .
+  (const_declaration))
+
+(source_file
+  (comment)+ @comment.documentation
+  .
+  (function_declaration))
+
+(source_file
+  (comment)+ @comment.documentation
+  .
+  (type_declaration))
+
+(source_file
+  (comment)+ @comment.documentation
+  .
+  (var_declaration))
+
+; Spell
+((interpreted_string_literal) @spell
+  (#not-has-parent? @spell import_spec))
--- queries/rust/highlights.scm
+++ queries/rust/highlights.scm
@@ -1,103 +1,161 @@
 ; Forked from https://github.com/tree-sitter/tree-sitter-rust
 ; Copyright (c) 2017 Maxim Sokolov
 ; Licensed under the MIT license.
-
 ; Identifier conventions
+(shebang) @keyword.directive
 
 (identifier) @variable
+
 ((identifier) @type
- (#lua-match? @type "^[A-Z]"))
+  (#lua-match? @type "^[A-Z]"))
+
 (const_item
   name: (identifier) @constant)
+
 ; Assume all-caps names are constants
 ((identifier) @constant
- (#lua-match? @constant "^[A-Z][A-Z%d_]*$"))
+  (#lua-match? @constant "^[A-Z][A-Z%d_]*$"))
 
 ; Other identifiers
-
 (type_identifier) @type
+
 (primitive_type) @type.builtin
-(field_identifier) @field
+
+(field_identifier) @variable.member
+
 (shorthand_field_initializer
-  (identifier) @field)
+  (identifier) @variable.member)
+
 (mod_item
- name: (identifier) @namespace)
+  name: (identifier) @module)
 
-(self) @variable.builtin
+[
+  (self)
+  "_"
+] @variable.builtin
 
-(loop_label ["'" (identifier)] @label)
-
+(label
+  [
+    "'"
+    (identifier)
+  ] @label)
 
 ; Function definitions
+(function_item
+  (identifier) @function)
 
-(function_item (identifier) @function)
-(function_signature_item (identifier) @function)
+(function_signature_item
+  (identifier) @function)
 
-(parameter (identifier) @parameter)
-(closure_parameters (_) @parameter)
+(parameter
+  [
+    (identifier)
+    "_"
+  ] @variable.parameter)
+
+(closure_parameters
+  (_) @variable.parameter)
 
 ; Function calls
 (call_expression
   function: (identifier) @function.call)
+
 (call_expression
   function: (scoped_identifier
-              (identifier) @function.call .))
+    (identifier) @function.call .))
+
 (call_expression
   function: (field_expression
     field: (field_identifier) @function.call))
 
 (generic_function
   function: (identifier) @function.call)
+
 (generic_function
   function: (scoped_identifier
     name: (identifier) @function.call))
+
 (generic_function
   function: (field_expression
     field: (field_identifier) @function.call))
 
 ; Assume other uppercase names are enum constructors
 ((field_identifier) @constant
- (#lua-match? @constant "^[A-Z]"))
+  (#lua-match? @constant "^[A-Z]"))
 
 (enum_variant
   name: (identifier) @constant)
 
 ; Assume that uppercase names in paths are types
 (scoped_identifier
-  path: (identifier) @namespace)
+  path: (identifier) @module)
+
 (scoped_identifier
- (scoped_identifier
-  name: (identifier) @namespace))
+  (scoped_identifier
+    name: (identifier) @module))
+
 (scoped_type_identifier
-  path: (identifier) @namespace)
+  path: (identifier) @module)
+
 (scoped_type_identifier
   path: (identifier) @type
   (#lua-match? @type "^[A-Z]"))
+
 (scoped_type_identifier
- (scoped_identifier
-  name: (identifier) @namespace))
+  (scoped_identifier
+    name: (identifier) @module))
+
 ((scoped_identifier
   path: (identifier) @type)
- (#lua-match? @type "^[A-Z]"))
+  (#lua-match? @type "^[A-Z]"))
+
 ((scoped_identifier
-    name: (identifier) @type)
- (#lua-match? @type "^[A-Z]"))
+  name: (identifier) @type)
+  (#lua-match? @type "^[A-Z]"))
+
+((scoped_identifier
+  name: (identifier) @constant)
+  (#lua-match? @constant "^[A-Z][A-Z%d_]*$"))
+
+((scoped_identifier
+  path: (identifier) @type
+  name: (identifier) @constant)
+  (#lua-match? @type "^[A-Z]")
+  (#lua-match? @constant "^[A-Z]"))
+
+((scoped_type_identifier
+  path: (identifier) @type
+  name: (type_identifier) @constant)
+  (#lua-match? @type "^[A-Z]")
+  (#lua-match? @constant "^[A-Z]"))
 
 [
   (crate)
   (super)
-] @namespace
+] @module
 
 (scoped_use_list
-  path: (identifier) @namespace)
+  path: (identifier) @module)
+
 (scoped_use_list
   path: (scoped_identifier
-            (identifier) @namespace))
-(use_list (scoped_identifier (identifier) @namespace . (_)))
-(use_list (identifier) @type (#lua-match? @type "^[A-Z]"))
-(use_as_clause alias: (identifier) @type (#lua-match? @type "^[A-Z]"))
+    (identifier) @module))
 
-;; Correct enum constructors
+(use_list
+  (scoped_identifier
+    (identifier) @module
+    .
+    (_)))
+
+(use_list
+  (identifier) @type
+  (#lua-match? @type "^[A-Z]"))
+
+(use_as_clause
+  alias: (identifier) @type
+  (#lua-match? @type "^[A-Z]"))
+
+; Correct enum constructors
 (call_expression
   function: (scoped_identifier
     "::"
@@ -106,118 +164,171 @@
 
 ; Assume uppercase names in a match arm are constants.
 ((match_arm
-   pattern: (match_pattern (identifier) @constant))
- (#lua-match? @constant "^[A-Z]"))
+  pattern: (match_pattern
+    (identifier) @constant))
+  (#lua-match? @constant "^[A-Z]"))
+
 ((match_arm
-   pattern: (match_pattern
-     (scoped_identifier
-       name: (identifier) @constant)))
- (#lua-match? @constant "^[A-Z]"))
+  pattern: (match_pattern
+    (scoped_identifier
+      name: (identifier) @constant)))
+  (#lua-match? @constant "^[A-Z]"))
 
 ((identifier) @constant.builtin
- (#any-of? @constant.builtin "Some" "None" "Ok" "Err"))
+  (#any-of? @constant.builtin "Some" "None" "Ok" "Err"))
 
-;; Macro definitions
+; Macro definitions
 "$" @function.macro
+
 (metavariable) @function.macro
-(macro_definition "macro_rules!" @function.macro)
 
-;; Attribute macros
-(attribute_item (attribute (identifier) @function.macro))
-(attribute (scoped_identifier (identifier) @function.macro .))
+(macro_definition
+  "macro_rules!" @function.macro)
 
-;; Derive macros (assume all arguments are types)
+; Attribute macros
+(attribute_item
+  (attribute
+    (identifier) @function.macro))
+
+(inner_attribute_item
+  (attribute
+    (identifier) @function.macro))
+
+(attribute
+  (scoped_identifier
+    (identifier) @function.macro .))
+
+; Derive macros (assume all arguments are types)
 ; (attribute
 ;   (identifier) @_name
 ;   arguments: (attribute (attribute (identifier) @type))
 ;   (#eq? @_name "derive"))
-
-;; Function-like macros
+; Function-like macros
 (macro_invocation
   macro: (identifier) @function.macro)
+
 (macro_invocation
   macro: (scoped_identifier
-           (identifier) @function.macro .))
+    (identifier) @function.macro .))
 
-
-
-;;; Literals
-
+; Literals
 [
   (line_comment)
   (block_comment)
 ] @comment @spell
 
+(line_comment
+  (doc_comment)) @comment.documentation
+
+(block_comment
+  (doc_comment)) @comment.documentation
+
 (boolean_literal) @boolean
+
 (integer_literal) @number
-(number.float_literal) @number.float
+
+(float_literal) @number.float
 
 [
   (raw_string_literal)
   (string_literal)
 ] @string
+
 (escape_sequence) @string.escape
+
 (char_literal) @character
 
-
-;;; Keywords
-
+; Keywords
 [
   "use"
   "mod"
-] @include
-(use_as_clause "as" @include)
+] @keyword.import
+
+(use_as_clause
+  "as" @keyword.import)
 
 [
-  "async"
-  "await"
   "default"
-  "dyn"
-  "enum"
-  "extern"
   "impl"
   "let"
-  "match"
   "move"
-  "pub"
-  "struct"
-  "trait"
-  "type"
-  "union"
   "unsafe"
   "where"
 ] @keyword
 
 [
- "ref"
- (mutable_specifier)
-] @type.qualifier
+  "enum"
+  "struct"
+  "union"
+  "trait"
+  "type"
+] @keyword.type
 
 [
- "const"
- "static"
-] @storageclass
+  "async"
+  "await"
+] @keyword.coroutine
 
-(lifetime ["'" (identifier)] @storageclass.lifetime)
+"try" @keyword.exception
+
+[
+  "ref"
+  "pub"
+  (mutable_specifier)
+  "const"
+  "static"
+  "dyn"
+  "extern"
+] @keyword.modifier
+
+(lifetime
+  "'" @keyword.modifier)
+
+(lifetime
+  (identifier) @attribute)
+
+(lifetime
+  (identifier) @attribute.builtin
+  (#any-of? @attribute.builtin "static" "_"))
 
 "fn" @keyword.function
+
 [
   "return"
   "yield"
 ] @keyword.return
 
-(type_cast_expression "as" @keyword.operator)
-(qualified_type "as" @keyword.operator)
+(type_cast_expression
+  "as" @keyword.operator)
 
-(use_list (self) @keyword)
-(scoped_use_list (self) @keyword)
-(scoped_identifier [(crate) (super) (self)] @keyword)
-(visibility_modifier [(crate) (super) (self)] @keyword)
+(qualified_type
+  "as" @keyword.operator)
+
+(use_list
+  (self) @module)
+
+(scoped_use_list
+  (self) @module)
+
+(scoped_identifier
+  [
+    (crate)
+    (super)
+    (self)
+  ] @module)
+
+(visibility_modifier
+  [
+    (crate)
+    (super)
+    (self)
+  ] @module)
 
 [
-  "else"
   "if"
-] @conditional
+  "else"
+  "match"
+] @keyword.conditional
 
 [
   "break"
@@ -225,14 +336,14 @@
   "in"
   "loop"
   "while"
-] @repeat
+] @keyword.repeat
 
 "for" @keyword
+
 (for_expression
-  "for" @repeat)
+  "for" @keyword.repeat)
 
-;;; Operators & Punctuation
-
+; Operators
 [
   "!"
   "!="
@@ -247,9 +358,9 @@
   "+="
   "-"
   "-="
-  "->"
   ".."
   "..="
+  "..."
   "/"
   "/="
   "<"
@@ -258,7 +369,6 @@
   "<="
   "="
   "=="
-  "=>"
   ">"
   ">="
   ">>"
@@ -272,16 +382,79 @@
   "||"
 ] @operator
 
-["(" ")" "[" "]" "{" "}"]  @punctuation.bracket
-(closure_parameters "|"    @punctuation.bracket)
-(type_arguments  ["<" ">"] @punctuation.bracket)
-(type_parameters ["<" ">"] @punctuation.bracket)
-(bracketed_type ["<" ">"] @punctuation.bracket)
-(for_lifetimes ["<" ">"] @punctuation.bracket)
+; Punctuation
+[
+  "("
+  ")"
+  "["
+  "]"
+  "{"
+  "}"
+] @punctuation.bracket
 
-["," "." ":" "::" ";"] @punctuation.delimiter
+(closure_parameters
+  "|" @punctuation.bracket)
 
-(attribute_item "#" @punctuation.special)
-(inner_attribute_item ["!" "#"] @punctuation.special)
-(macro_invocation "!" @function.macro)
-(empty_type "!" @type.builtin)
+(type_arguments
+  [
+    "<"
+    ">"
+  ] @punctuation.bracket)
+
+(type_parameters
+  [
+    "<"
+    ">"
+  ] @punctuation.bracket)
+
+(bracketed_type
+  [
+    "<"
+    ">"
+  ] @punctuation.bracket)
+
+(for_lifetimes
+  [
+    "<"
+    ">"
+  ] @punctuation.bracket)
+
+[
+  ","
+  "."
+  ":"
+  "::"
+  ";"
+  "->"
+  "=>"
+] @punctuation.delimiter
+
+(attribute_item
+  "#" @punctuation.special)
+
+(inner_attribute_item
+  [
+    "!"
+    "#"
+  ] @punctuation.special)
+
+(macro_invocation
+  "!" @function.macro)
+
+(never_type
+  "!" @type.builtin)
+
+(macro_invocation
+  macro: (identifier) @keyword.exception
+  "!" @keyword.exception
+  (#eq? @keyword.exception "panic"))
+
+(macro_invocation
+  macro: (identifier) @keyword.exception
+  "!" @keyword.exception
+  (#contains? @keyword.exception "assert"))
+
+(macro_invocation
+  macro: (identifier) @keyword.debug
+  "!" @keyword.debug
+  (#eq? @keyword.debug "dbg"))
